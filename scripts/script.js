ee647a8a74e9e663b82703cadcfaae26716e62b1
musabadnan08 <musabwbdeft19@gmail.com>
2025-01-07 19:49:01 +0600

    added different sections for pets

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,26 @@
 console.log("hello")
+
+const loadCategories = () => {
+    fetch("https://openapi.programming-hero.com/api/peddy/categories")
+        .then((res) => res.json())
+        .then((data) => displayCategories(data.categories))
+        .catch((error) => console.log(error))
+}
+loadCategories()
+
+const displayCategories = (categories) => {
+    const categoryContainer = document.getElementById("categories")
+
+    categories.forEach((item) => {
+        console.log(item)
+
+        const button = document.createElement("button")
+        button.classList = "btn";
+        button.innerHTML =
+            `<div class="flex justify-center items-center">
+                <img class="w-full" src=${item.category_icon}/> ${item.category}
+            </div>`
+
+        categoryContainer.append(button);
+    })
+}
