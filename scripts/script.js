dcb7f6ed2682f7a03df695e790048bf859c1359e
musabadnan08 <musabwbdeft19@gmail.com>
2025-01-08 19:54:40 +0600

    added modal feature to show details about the specific pet

--- scripts/script.js
+++ scripts/script.js
@@ -102,16 +102,68 @@ const displayCards = (pets) => {
             <p class="px-2">${card.price}</p>
             </div>
 
-            <div class="card-actions justify-between">
-                <button class="btn btn-primary">like</button>
-                <button class="btn btn-primary">adopt</button>
-                <button class="btn btn-primary">details</button>
+            <div class="card-actions justify-between py-3">
+                <button class="btn bg-white text-green-900"><img class=w-7 src="${"https://img.icons8.com/?size=50&id=24816&format=png"}"/></button>
+                <button class="btn bg-white text-green-900 text-xl font-bold">Adopt</button>
+                <button onclick="loadDetails('${card.petId}')" class="btn bg-white text-green-900 text-xl font-bold">Details</button>
             </div>
         </div>`
         cardContainer.append(allCards);
     })
 }
-// 
+const loadDetails = async (petId) => {
+    console.log(petId)
+    const uri = `https://openapi.programming-hero.com/api/peddy/pet/${petId}`
+    const res = await fetch(uri);
+    const data = await res.json();
+    displayDetails(data.petData)
+}
+const displayDetails = (petData) => {
+    console.log(petData);
+    const property = "breed";
+    const property2 = "date_of_birth";
+    const property3 = "gender";
+    const modalContainer = document.getElementById("modal-content")
+    modalContainer.innerHTML = `
+    <div class="grid grid-cols-1">
+    <div class="h-[200px]"><img class="h-full w-full object-cover" src="${petData.image}"></div>
+    <div>
+    <h2 class="text-xl font-bold py-1">${petData.pet_name}</h2>
+    <div class="flex justify-between">
+    <div>
+    <div class="flex">
+        <img class=w-5 src="${"https://img.icons8.com/?size=64&id=k5wELv7XvOR6&format=png"}"/>
+        ${property in petData ? `<p class="px-2">${petData.breed}</p>` : `<p class="px-2">Breed Name Not Available</p>`}
+    </div>
+    <div class="flex">
+        <img class=w-5 src="${"https://img.icons8.com/?size=64&id=JQyh0I18IaES&format=png"}"/>
+        ${property2 in petData && petData[property2] !== null ? `<p class="px-2">${petData.date_of_birth}</p>` : `<p class="px-2">Date of Birth Not Available</p>`}
+    </div>
+    <div class="flex">
+        <img class=w-5 src="${"https://img.icons8.com/?size=64&id=vgKCn1iIkmp9&format=png"}"/>
+        ${property3 in petData ? `<p class="px-2">${petData.gender}</p>` : `<p class="px-2">Gender is Undefined</p>`}
+    </div>
+    </div>
+    <div>
+    <div class="flex">
+        <img class=w-5 src="${"https://img.icons8.com/?size=64&id=xuQVdwxnqTRn&format=png"}"/>
+        <p class="px-2">${petData.price}</p>
+    </div>
+        <div class="flex">
+        <img class=w-5 src="${"https://img.icons8.com/?size=64&id=k5wELv7XvOR6&format=png"}"/>
+        <p class="px-2">Vaccination Status: ${petData.vaccinated_status}</p>
+    </div>
+    </div>
+    </div>
+    <hr class="mt-3">
+    <h4 class="text-xl font-bold py-1">Detailed Information</h4>
+    <p>${petData.pet_details}</p>
+    </div>
+    </div>
+    `
+    document.getElementById("customModal").showModal();
+}
+//
 const objDemo = {
     "petId": 12,
     "breed": "Poodle",
